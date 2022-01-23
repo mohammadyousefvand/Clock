cff3d6c366e1f5bf4590dc2537cc73eada69a510
pc <www.mmhmdmm83@gmail.com>
2022-01-23 11:04:26 +0330

    add week , month , day and year

--- js/app.js
+++ js/app.js
@@ -7,21 +7,35 @@ const hrAnalog = $.querySelector('.hr-analog')
 const minAnalog = $.querySelector('.min-analog')
 const secAnalog = $.querySelector('.sec-analog')
 const amOrPm = $.querySelector('.amorpm')
+const week = $.querySelector('.week')
+const year = $.querySelector('.year')
+const month = $.querySelector('.month')
+const day = $.querySelector('.day')
 
+const weekname = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const monthname = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 // Digital Clock
 
 setInterval(function () {
 
-   let timedigit = new Date()
+    let timedigit = new Date()
 
-   let hourdigit = timedigit.getHours()
-   let minutedigit = timedigit.getMinutes()
-   let seconddigit = timedigit.getSeconds()
+    let hourdigit = timedigit.getHours()
+    let minutedigit = timedigit.getMinutes()
+    let seconddigit = timedigit.getSeconds()
+    let yearDate = timedigit.getFullYear()
+    let dayDate = timedigit.getDate()
+    let weekDate = weekname[timedigit.getDay()]
+    let monthDate = monthname[timedigit.getMonth()]
 
     hrDigit.innerHTML = hourdigit
     minDigit.innerHTML = minutedigit
     secDigit.innerHTML = seconddigit
+    year.innerHTML = yearDate
+    day.innerHTML = dayDate
+    week.innerHTML = weekDate
+    month.innerHTML = monthDate
 
     if (hourdigit < 10) {
         hrDigit.innerHTML = '0' + hourdigit
@@ -29,7 +43,7 @@ setInterval(function () {
     if (minutedigit < 10) {
         minDigit.innerHTML = '0' + minutedigit
     }
-    if (seconddigit < 10){
+    if (seconddigit < 10) {
         secDigit.innerHTML = '0' + seconddigit
     }
 
@@ -40,22 +54,24 @@ setInterval(function () {
         amOrPm.innerHTML = 'Pm'
     }
 
+
+
 }, 1000)
 
-setInterval( function () {
+setInterval(function () {
 
-   let timeAnalog = new Date()
+    let timeAnalog = new Date()
 
-   let hourAnalog = timeAnalog.getHours()
-   let minuteAnalog = timeAnalog.getMinutes()
-   let secondAnalog = timeAnalog.getSeconds()
+    let hourAnalog = timeAnalog.getHours()
+    let minuteAnalog = timeAnalog.getMinutes()
+    let secondAnalog = timeAnalog.getSeconds()
 
     let calcHr = (hourAnalog * 30) + (minuteAnalog / 2);
     let calcMin = (minuteAnalog * 6) + (secondAnalog / 10);
-    let calcSec = secondAnalog*6;
+    let calcSec = secondAnalog * 6;
 
     hrAnalog.style.transform = 'rotate(' + calcHr + 'deg)'
     minAnalog.style.transform = 'rotate(' + calcMin + 'deg)'
     secAnalog.style.transform = 'rotate(' + calcSec + 'deg)'
 
-} , 1000)
+}, 1000)
